4fefc3b486d0cc73527ac4904c9a79e816cd2b22
Martin Di Paola <martinp.dipaola@gmail.com>
2018-09-17 12:52:02 +0000

    Own implementation of Entropy (scipy.stats is optional now)

--- cryptonita/stats.py
+++ cryptonita/stats.py
@@ -1,5 +1,57 @@
 import collections
 
+import math
+def _py_entropy(pk, qk=None, base=None):
+    '''
+        >>> from cryptonita.stats import _py_entropy
+
+        Input normalized
+        >>> _py_entropy([0.3, 0.5, 0.2])
+        1.0296530140645<...>
+
+        Input not normalized
+        >>> _py_entropy([1.3, 2.5, 3.2])
+        1.0382125826451<...>
+
+        Different log base (e by default)
+        >>> _py_entropy([1.3, 2.5, 3.2], base=7)
+        0.5335357252487<...>
+
+        Two inputs (Kullback-Leibler divergence)
+        >>> _py_entropy([0.3, 0.5, 0.2], [0.1, 0.1, 0.8])
+        0.8570437705935<...>
+
+        Two inputs: not normalized - normalized
+        >>> _py_entropy([1.3, 2.5, 3.2], [0.1, 0.1, 0.8])
+        0.3137706627238<...>
+
+        Two inputs: normalized - not normalized
+        >>> _py_entropy([0.3, 0.5, 0.2], [1.1, 2.1, 3.8])
+        0.24969519533828<...>
+
+        Two inputs: different base.
+        >>> _py_entropy([1.3, 2.5, 3.2], [0.1, 0.1, 0.8], base=7)
+        0.1612462234580<...>
+
+    '''
+    log = math.log
+    if base == None:
+        base = math.e
+
+    if qk == None:
+        np = sum(pk)
+        return log(np, base) - sum((p * log(p, base)) for p in pk) / np
+    else:
+        np = sum(pk)
+        nq = sum(qk)
+        return - log(np/nq, base) + sum((p * log(p/q, base)) for p, q in zip(pk, qk)) / np
+
+try:
+    from scipy.stats import entropy
+except ImportError:
+    entropy = _py_entropy
+
+
 class SequenceStatsMixin:
     def freq(self):
         return collections.Counter(self)
@@ -8,3 +60,6 @@ class SequenceStatsMixin:
         elems, _ = zip(*self.freq().most_common(n))
         return elems
 
+    def entropy(self):
+        freq = list(self.freq().values())
+        return entropy(freq)
