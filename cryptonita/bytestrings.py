326ba341fbfb59bdbf326763ede58dc876b1bb27
Martin Di Paola <martinp.dipaola@gmail.com>
2020-08-21 03:57:29 +0000

    Support int() and map() for ImmutableByteString
    Call int(b) to get the integer represed by the byte string b. It only
    works if len(b) == 1.
    
    Call b.map(...) to return another byte string with some or all of its
    bytes translated to others, deleting some others. This is an extension
    of bytes.translate

--- cryptonita/bytestrings.py
+++ cryptonita/bytestrings.py
@@ -6,6 +6,7 @@ from cryptonita.mixins import (
         )
 
 import numpy as np
+from collections.abc import Iterable, Callable
 
 '''
 >>> from cryptonita import B           # byexample: +timeout=10
@@ -77,6 +78,187 @@ class ImmutableByteString(SequenceMixin, ByteStatsMixin, SequenceStatsMixin, byt
 
     def fhex(self, n=8):
         return super().hex()[:n]
+
+    def __int__(self):
+        ''' See the single byte as an interger.
+
+            >>> int(B(4))
+            4
+
+            This is a shortcut for
+
+            >>> B(4)[0] == int(B(4))
+            True
+
+            Empty byte strings or strings of more than one byte will fail
+
+            >>> int(B(''))
+            Traceback<...>
+            ValueError: The byte string cannot be seen as an integer of 1 byte
+
+            >>> int(B(b'AB'))
+            Traceback<...>
+            ValueError: The byte string cannot be seen as an integer of 1 byte
+        '''
+        if len(self) != 1:
+            raise ValueError("The byte string cannot be seen as an integer of 1 byte")
+
+        return self[0]
+
+    def map(self, table, table2=None, fill=-1, delete=set()):
+        ''' Translate the byte string replacing each byte
+            by the byte given in <table>, optionally deleting
+            bytes from the <delete> set and filling the missing
+            ones with <fill>.
+
+            This is an enhanced version of bytes.translate()
+            and str.translate().
+
+            The <table> can be a bytes-like sequence of 256
+            bytes and the mapping is based on position.
+
+            Given the table:
+
+            >>> t = B(b'ABC').pad(256, 'zeros')
+
+            The bytes 0, 1 and 2 of the following byte string
+            are translated to A, B and C respectively
+
+            >>> s = B([0, 1, 2])
+            >>> s.map(t)
+            b'ABC'
+
+            But other mapping are possible.
+
+            If <table> is a dictionary, the mapping is by value:
+            each byte is looked up in the dictionary and the value
+            returned is its replacement.
+
+            >>> s.map({0: b'A', 1: b'B', 2: b'C'})
+            b'ABC'
+
+            If two tables are given, they must have the same length
+            and the mapping is by value: the ith value in the table
+            of the left is mapped to the ith value of the right.
+
+            >>> s.map([0, 1, 2], [b'A', b'B', b'C'])
+            b'ABC'
+
+            An iterator and a callable are supported too. The iterator
+            must return tuples to map one item to another and the
+            callable must accept one item and return its replacement
+
+            >>> s.map(zip([0, 1, 2], [b'A', b'B', b'C']))
+            b'ABC'
+
+            >>> s.map(lambda n: [b'A', b'B', b'C'][n])
+            b'ABC'
+
+            The callables can raise LookupError or IndexError to signal
+            that there is no a valid mapping for the given byte (see
+            "default mapping" below). Returning None will mark the bytes
+            for deletion (see "delete bytes" below).
+
+            Note: the callable will be called 256 times to generate the
+            mapping for all the possible bytes. It will *not* be called
+            per each byte of <self>. For that use the standar Python
+            function <map> or a for-loop.
+
+            For all the possible mappings, inputs and outputs can be
+            integers (0 <= i < 256), bytes, bytearrays or
+            immutable or mutable byte strings (of length 1).
+
+            With the exception of a single byte string of 256 values
+            as a table, all the mappings are "incomplete": they define
+            how to map a subset of the possible values that a byte string
+            could have (256 to be precise).
+
+            We can define a default mapping for them:
+
+             - we can replace them by a default (fill = <default>)
+             - we can leave them as they are (fill = -1)
+             - we can delete them (fill = None)
+
+            >>> s.map([(1, b'B')], fill=0x40)
+            b'@B@'
+
+            >>> s.map([(1, b'B')], fill=-1)
+            b'\x00B\x02'
+
+            >>> s.map([(1, b'B')], fill=None)
+            b'B'
+
+            Deleting some bytes is possible not only with fill=None but
+            returning None from a callable
+
+            >>> s.map(lambda x: b'B' if x == 1 else None)
+            b'B'
+
+            Also, we can define the set of bytes to delete from the
+            <delete> parameter:
+
+            >>> s.map([], delete={0x0, 0x2})
+            b'\x01'
+
+        '''
+        if isinstance(table, dict):
+            table = table.items()
+
+        if table2 is not None:
+            table = zip(table, table2)
+
+        if isinstance(table, Callable):
+            func = table
+            table = []
+            for b in range(256):
+                try:
+                    v = func(b)
+                except LookupError:
+                    v = b
+                except IndexError:
+                    v = b
+
+                table.append((b, v))
+
+        def as_int(obj):
+            if isinstance(obj, (bytes, bytearray)):
+                obj = ImmutableByteString(obj)
+            return int(obj)
+
+        # by default do not delete anything. this is compatible with the
+        # default of bytes.translate.
+        delete_set = set(delete)
+        if isinstance(table, Iterable) and not isinstance(table, bytes):
+            if fill is None:
+                # delete all of them by default
+                delete_set = set(range(256))
+
+            if fill == -1 or fill is None:
+                tmp = list(range(256))
+            else:
+                tmp = [as_int(fill)] * 256
+
+            for ix, v in table:
+                # normalize the items as (int, int) values
+                ix = as_int(ix)
+
+                if v is None:
+                    # None means delete it
+                    delete_set.add(ix)
+                    v = 0
+                else:
+                    # if it is not None means that we don't want to
+                    # delete it. If the default is delete (fill is None),
+                    # removing the item from the set makes sense
+                    delete_set.discard(ix)
+                    v = as_int(v)
+
+                tmp[ix] = v
+
+            table = bytes(tmp)
+
+        deletechars = bytes(delete_set)
+        return super().translate(table, deletechars)
 
 class MutableByteString(MutableSequenceMixin, bytearray):
     ''' Enhanced version of a mutable byte string.
