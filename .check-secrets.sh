fa9cae29ba3e386e7764af4ed804e6bb54e6e918
Martin Di Paola <martinp.dipaola@gmail.com>
2023-01-14 12:47:14 -0300

    Fix CI

--- .check-secrets.sh
+++ .check-secrets.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-. .check-secrets-impl.sh
+if [ -x .check-secrets-impl.sh ]; then
+    . .check-secrets-impl.sh
+elif [ -f .check-secrets-impl.sh ]; then
+    exit 1
+else
+    exit 0
+fi
